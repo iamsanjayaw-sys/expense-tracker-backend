7dea7e5a4b0ba186f6a926a24b0599c4cf9abf3b
iamsanjayaw-sys <iamsanjaya.w@gmail.com>
2026-01-28 16:56:56 +0530

    created backend aulth user registration part
    successfully created and working
    aulthColtroller.js
    aulthRoutes.js
    aulthMiddleware.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
+const authRoutes = require("./routes/authRoutes");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
